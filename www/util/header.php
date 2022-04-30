1bdd506fa2949031978896f7a7fdf31ad228de30
irrelevant <irrelevant@cityzenz.de>
2022-04-30 19:09:14 +0200

    Implemented header insertion in header.php

---
+++ www/util/header.php
@@ -0,0 +1,24 @@
+<?php
+
+function stringBetweenTwoStrings($str, $starting_word, $ending_word){
+	$substring_start = strpos($str, $starting_word);
+	$substring_start += strlen($starting_word);
+	$size = strpos($str, $ending_word, $substring_start) - $substring_start;
+	return substr($str, $substring_start, $size);
+}
+
+function insertHeader($html){
+	$header_file = file_get_contents(__DIR__ . '/../static/header.html');
+	$header = '<header>';
+	$header .= stringBetweenTwoStrings($header_file, '<header>', '</header>');
+	$header .= '</header>';
+
+	$header_file = file_get_contents(__DIR__ . '/../static/header.html');
+	$styles = stringBetweenTwoStrings($header_file, '<!-- header styles start -->', '<!-- header styles end -->');
+
+	$html = str_replace('<!-- header stylesheets -->', $styles, $html);
+	$html = str_replace('<header></header>', $header, $html);
+	return $html;
+}
+
+?>
