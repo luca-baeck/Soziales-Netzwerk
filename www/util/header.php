6e4e9f3de8439c7254c3b128ebbb4370dcfa6ae3
irrelevant <irrelevant@cityzenz.de>
2022-05-05 11:53:01 +0200

    Changed the path of the html files

--- www/util/header.php
+++ www/util/header.php
@@ -8,12 +8,12 @@ function stringBetweenTwoStrings($str, $starting_word, $ending_word){
 }
 
 function insertHeader($html){
-	$header_file = file_get_contents(__DIR__ . '/../static/header.html');
+	$header_file = file_get_contents(__DIR__ . './static/header.html');
 	$header = '<header>';
 	$header .= stringBetweenTwoStrings($header_file, '<header>', '</header>');
 	$header .= '</header>';
 
-	$header_file = file_get_contents(__DIR__ . '/../static/header.html');
+	$header_file = file_get_contents(__DIR__ . './static/header.html');
 	$styles = stringBetweenTwoStrings($header_file, '<!-- header styles start -->', '<!-- header styles end -->');
 
 	$html = str_replace('<!-- header stylesheets -->', $styles, $html);
