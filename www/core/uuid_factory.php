a649ce7cda2df0715f7c1c905b4b3ea9db255b19
irrelevant <irrelevant@cityzenz.de>
2022-05-05 13:02:04 +0200

    Changed UUID-format from 32 to 36 characters

--- www/core/uuid_factory.php
+++ www/core/uuid_factory.php
@@ -15,8 +15,9 @@ class UUIDFactory extends IDFactory{
 		$data[8] = chr(ord($data[8]) & 0x3f | 0x80);
 
 		// Output the 36 character UUID.
-		// return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-		return vsprintf('%s%s%s%s%s%s%s%s', str_split(bin2hex($data), 4));
+		return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+		// Output the 32 character UUID.
+		// return vsprintf('%s%s%s%s%s%s%s%s', str_split(bin2hex($data), 4));
 	}
 }
 
