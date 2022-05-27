2792ea01182d7c95a485ad1c887479284a201972
irrelevant <irrelevant@cityzenz.de>
2022-05-27 18:14:06 +0200

    Moved the method stringBetweenTwoStrings in new class StringUtils

--- www/util/header.php
+++ www/util/header.php
@@ -1,18 +1,13 @@
 <?php
 
-function stringBetweenTwoStrings($str, $starting_word, $ending_word){
-	$substring_start = strpos($str, $starting_word);
-	$substring_start += strlen($starting_word);
-	$size = strpos($str, $ending_word, $substring_start) - $substring_start;
-	return substr($str, $substring_start, $size);
-}
+require_once('./util/string.php');
 
 function insertHeader($html){
 	$header_file = file_get_contents('./static/header.html');
 	$header = '<header>';
-	$header .= stringBetweenTwoStrings($header_file, '<header>', '</header>');
+	$header .= StringUtils::stringBetweenTwoStrings($header_file, '<header>', '</header>');
 	$header .= '</header>';
-	$styles = stringBetweenTwoStrings($header_file, '<!-- header styles start -->', '<!-- header styles end -->');
+	$styles = StringUtils::stringBetweenTwoStrings($header_file, '<!-- header styles start -->', '<!-- header styles end -->');
 
 	$html = str_replace('<!-- header stylesheets -->', $styles, $html);
 	$html = str_replace('<header></header>', $header, $html);
---
+++ www/util/string.php
@@ -0,0 +1,14 @@
+<?php
+
+class StringUtils{
+
+	public static function stringBetweenTwoStrings($str, $starting_word, $ending_word){
+		$substring_start = strpos($str, $starting_word);
+		$substring_start += strlen($starting_word);
+		$size = strpos($str, $ending_word, $substring_start) - $substring_start;
+		return substr($str, $substring_start, $size);
+	}
+	
+}
+
+?>
