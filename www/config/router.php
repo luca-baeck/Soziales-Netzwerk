f83bad1881b7e8da01f74386f419f8a0736d84f8
irrelevant <irrelevant@cityzenz.de>
2022-05-30 23:20:56 +0200

    Added fileadmin to ctrl files in router config

--- www/config/router.php
+++ www/config/router.php
@@ -2,6 +2,7 @@
 
 class RouterConfig{
 	const PUBLIC_CTRL_FILES = array(
+		'fileadmin',
 		'land',
 		'login',
 		'register',
@@ -21,6 +22,7 @@ class RouterConfig{
 	const ALL_CTRL_FILES = array(
 		'create',
 		'feed',
+		'fileadmin',
 		'land',
 		'login',
 		'register',
