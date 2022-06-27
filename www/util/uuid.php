2d4058bbe0b6a5ecb737d24ad910b9d4d74bd145
irrelevant <irrelevant@cityzenz.de>
2022-06-27 11:40:01 +0200

    Switched order of expand and strip functions

--- www/util/uuid.php
+++ www/util/uuid.php
@@ -2,18 +2,6 @@
 
 class UUIDUtils{
 
-	public static function strip(string $uuid): string{
-		if(empty($uuid)){
-			return 'ERROR: empty UUID';
-		}else if(strlen($uuid) == 32){
-			return $uuid;
-		}else if(strlen($uuid) != 36){
-			return 'ERROR: wrong format';
-		}else{
-			return substr($uuid, 0, 8) . substr($uuid, 9, 13) . substr($uuid, 14, 18) . substr($uuid, 19, 23) . substr($uuid, 24, 36);
-		}
-	}
-
 	public static function expand(string $uuid) :string{
 		if(empty($uuid)){
 			return 'ERROR: empty UUID';
@@ -23,6 +11,18 @@ class UUIDUtils{
 			return 'ERROR: wrong format';
 		}else{
 			return substr($uuid, 0, 8) . '-' . substr($uuid, 8, 12) . '-' . substr($uuid, 12, 16) . '-' . substr($uuid, 16, 20) . '-' . substr($uuid, 20, 32);
+		}
+	}
+
+	public static function strip(string $uuid): string{
+		if(empty($uuid)){
+			return 'ERROR: empty UUID';
+		}else if(strlen($uuid) == 32){
+			return $uuid;
+		}else if(strlen($uuid) != 36){
+			return 'ERROR: wrong format';
+		}else{
+			return substr($uuid, 0, 8) . substr($uuid, 9, 13) . substr($uuid, 14, 18) . substr($uuid, 19, 23) . substr($uuid, 24, 36);
 		}
 	}
 
