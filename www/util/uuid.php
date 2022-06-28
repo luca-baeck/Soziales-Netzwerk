b858a0f74c74a0b4a1f6f84059f78d4f260df7c1
irrelevant <irrelevant@cityzenz.de>
2022-06-28 15:45:43 +0200

    Fixed stupid mistake (Replaced end position by size)

--- www/util/uuid.php
+++ www/util/uuid.php
@@ -10,7 +10,7 @@ class UUIDUtils{
 		}else if(strlen($uuid) != 32){
 			return 'ERROR: wrong format';
 		}else{
-			return substr($uuid, 0, 8) . '-' . substr($uuid, 8, 12) . '-' . substr($uuid, 12, 16) . '-' . substr($uuid, 16, 20) . '-' . substr($uuid, 20, 32);
+			return substr($uuid, 0, 8) . '-' . substr($uuid, 8, 4) . '-' . substr($uuid, 12, 4) . '-' . substr($uuid, 16, 4) . '-' . substr($uuid, 20, 12);
 		}
 	}
 
@@ -30,7 +30,7 @@ class UUIDUtils{
 		}else if(strlen($uuid) != 36){
 			return 'ERROR: wrong format';
 		}else{
-			return substr($uuid, 0, 8) . substr($uuid, 9, 13) . substr($uuid, 14, 18) . substr($uuid, 19, 23) . substr($uuid, 24, 36);
+			return substr($uuid, 0, 8) . substr($uuid, 9, 4) . substr($uuid, 14, 4) . substr($uuid, 19, 4) . substr($uuid, 24, 12);
 		}
 	}
 
