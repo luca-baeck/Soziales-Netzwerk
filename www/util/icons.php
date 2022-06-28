90fb156153a52af347241964b598f30f19a42fa6
irrelevant <irrelevant@cityzenz.de>
2022-06-28 18:59:54 +0200

    Added method to include loads of different sizes for icons

---
+++ www/util/icons.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('./util/string.php');
+
+class Icons{
+	public function insert(string $html): string{
+		$icons_file = file_get_contents('./view/snippets/html/icons.html');
+		$icons = StringUtils::stringBetweenTwoStrings($icons_file, '<!-- icons begin -->', '<!-- icons end -->');
+
+		$html = str_replace('<!-- icons placeholder -->', $icons, $html);
+		return $html;
+	}
+}
+
+?>
