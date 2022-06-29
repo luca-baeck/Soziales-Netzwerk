09f7642009b7a7e4fc86f57b8b9e2081781e0d0d
irrelevant <irrelevant@cityzenz.de>
2022-06-29 23:35:39 +0200

    Added method stringStartsWith() to StringUtils

--- www/util/string.php
+++ www/util/string.php
@@ -8,6 +8,10 @@ class StringUtils{
 		$size = strpos($str, $ending_word, $substring_start) - $substring_start;
 		return substr($str, $substring_start, $size);
 	}
+
+	public static function stringStartsWith(string $haystack, string $needle): bool{
+		return substr_compare($haystack, $needle, 0, strlen($needle)) == 0;
+	}
 	
 }
 
