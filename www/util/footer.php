c51cc3bcc104314461aedef2ea6a443140d6262e
irrelevant <irrelevant@cityzenz.de>
2022-06-30 00:36:25 +0200

    Changed 'start' to 'begin'

--- www/util/footer.php
+++ www/util/footer.php
@@ -6,7 +6,7 @@ class Footer{
 	public static function insert($html){
 		$footer_file = file_get_contents('./view/snippets/html/footer.html');
 		$footer = StringUtils::stringBetweenTwoStrings($footer_file, '<!-- footer begin -->', '<!-- footer end -->');
-		$styles = StringUtils::stringBetweenTwoStrings($footer_file, '<!-- footer styles start -->', '<!-- footer styles end -->');
+		$styles = StringUtils::stringBetweenTwoStrings($footer_file, '<!-- footer styles begin -->', '<!-- footer styles end -->');
 
 		$html = str_replace('<!-- footer stylesheets -->', $styles, $html);
 		$html = str_replace('<!-- footer placeholder -->', $footer, $html);
