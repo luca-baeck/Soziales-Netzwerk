e99b83e46e13a7ed6c6824b1947511a48cd83277
irrelevant <irrelevant@cityzenz.de>
2022-07-01 20:44:58 +0200

    Added element insertion

---
+++ www/util/element.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once('./data/post.php');
+require_once('./data/user.php');
+require_once('./util/string.php');
+
+class ElementUtils{
+
+	private static string $element_file;
+
+	public static function insertElement(string $uuid, $html, ?bool $allowFullscreen = true, ?User $user = NULL): string{
+		$post = new Post($uuid);
+		if(!isset($user)){
+			$user = new User($post->getCreatorUUID());
+		}
+		$element = StringUtils::stringBetweenTwoStrings($element_file, '<!-- element begin -->', '<!-- element end -->');
+
+		$element = str_replace('sticker-src', $post->getStickerURL(), $element);
+		$element = str_replace('<!-- post content -->', $post->getContent(), $element);
+		$element = str_replace('creator-handle', $post->getHandle(), $element);
+		$element = str_replace('profile-picture-src', $user->getProfilePictureURL(), $element);
+		$element = str_replace('<!-- creator name -->', $user->getName(), $element);
+		$element = str_replace('<!-- creator handle -->', $user->getHandle(), $elements);
+		$element = str_replace('<!-- creation date -->', $post->getCreationTime()->format('Y.m.d'), $element);
+
+		$html = str_replace('<!-- element placeholder -->', $element, $html);
+		return $html;
+	}
+
+	public static function insertElementStyles($html): string{
+		if(!isset($element_file)){
+			$element_file = file_get_contents('./view/snippets/html/element.html');
+		}
+		$styles = StringUtils::stringBetweenTwoStrings($element_file, '<!-- element styles begin -->', '<!-- element styles end -->');
+
+		$overlay = StringUtils::stringBetweenTwoStrings($element_file, '<!-- overlay begin -->', '<!-- overlay end -->');
+
+		$html = str_replace('<!-- overlay placeholder -->', $overlay, $html);
+
+		$html = str_replace('<!-- element stylesheets -->', $styles, $html);
+		return $html;
+	}
+
+}
+
+?>
